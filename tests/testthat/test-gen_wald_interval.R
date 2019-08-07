f9e1df4cdc18a4cb4b65bcf4012d3074b719bf96
Brian Williamson <brianw26@uw.edu>
2019-08-07 14:59:34 -0700

    change tests to run on new example data

--- tests/testthat/test-gen_wald_interval.R
+++ tests/testthat/test-gen_wald_interval.R
@@ -3,19 +3,13 @@
 library("testthat")
 library("rstan")
 library("paramedic")
-data(simple_example_data)
-q <- 3
-q_obs <- 2
-## process the data
-processed_data <- paramedic::process_data(full_data = simple_example_data, rel_inds = 1:q,
-                               abs_inds = (q + 1):(q + q_obs),
-                               abs_plus_rel_inds = 1:q_obs,
-                               regex_thr = "", regex_abs = "_cps", llod = 0,
-                               m_min = 1000, div_num = 1)
+data(example_16S_data)
+data(example_qPCR_data)
+
 ## run paramedic (with a *very* small number of iterations, for illustration only)
-mod <- paramedic::run_paramedic(W = processed_data$br, V = processed_data$qpcr,
-                            stan_model = "src/stan_files/variable_efficiency.stan", n_iter = 30, n_burnin = 25, n_chains = 1, stan_seed = 4747,
-                            params_to_save = c("mu", "Sigma", "beta", "e"))
+mod <- paramedic::run_paramedic(W = example_16S_data, V = example_qPCR_data,
+                                stan_model = "src/stan_files/variable_efficiency.stan", n_iter = 30, n_burnin = 25, n_chains = 1, stan_seed = 4747,
+                                params_to_save = c("mu", "Sigma", "beta", "e"))
 ## get model summary
 mod_summ <- rstan::summary(mod, probs = c(0.025, 0.975))$summary
 ## get samples
@@ -23,7 +17,7 @@ mod_samps <- rstan::extract(mod)
 test_that("wald intervals work", {
   summs <- extract_posterior_summaries(stan_mod = mod_summ, stan_samps = mod_samps, q = q, taxa_of_interest = 1:3, mult_num = 1, level = 0.95, interval_type = "wald")
   ## check that I have proper dimension of intervals
-  expect_equal(dim(summs$pred_intervals), c(dim(processed_data$br)[1], 2, length(1:3)))
+  expect_equal(dim(summs$pred_intervals), c(dim(example_16S_data)[1], 2, length(1:3)))
   ## check that the intervals cover the observed qPCR for taxon 1
-  expect_equal(mean(summs$pred_intervals[,,1][,1] <= processed_data$qpcr[, 1] & summs$pred_intervals[,,1][,2] >= processed_data$qpcr[, 1]), 0.95, tolerance = 0.1)
+  expect_equal(mean(summs$pred_intervals[,,1][,1] <= example_qPCR_data[, 1] & summs$pred_intervals[,,1][,2] >= example_qPCR_data[, 1]), 0.95, tolerance = 0.1)
 })
--- tests/testthat/test-run_paramedic.R
+++ tests/testthat/test-run_paramedic.R
@@ -3,24 +3,19 @@
 library("testthat")
 library("rstan")
 library("paramedic")
-data(simple_example_data)
-q <- 3
-q_obs <- 2
-## process the data
-processed_data <- paramedic::process_data(full_data = simple_example_data, rel_inds = 1:q,
-                               abs_inds = (q + 1):(q + q_obs),
-                               abs_plus_rel_inds = 1:q_obs,
-                               regex_thr = "", regex_abs = "_cps", llod = 0,
-                               m_min = 1000, div_num = 1)
+data(example_16S_data)
+data(example_qPCR_data)
+
+
 ## run paramedic (with a *very* small number of iterations, for illustration only)
 test_that("paramedic works", {
-  mod <- paramedic::run_paramedic(W = processed_data$br, V = processed_data$qpcr,
-                              stan_model = "src/stan_files/variable_efficiency.stan", n_iter = 30, n_burnin = 25, n_chains = 1, stan_seed = 4747,
-                              params_to_save = c("mu", "Sigma", "beta", "e"))
+  mod <- paramedic::run_paramedic(W = example_16S_data, V = example_qPCR_data,
+                                  stan_model = "src/stan_files/variable_efficiency.stan", n_iter = 30, n_burnin = 25, n_chains = 1, stan_seed = 4747,
+                                  params_to_save = c("mu", "Sigma", "beta", "e"))
   ## get model summary
   mod_summ <- rstan::summary(mod, probs = c(0.025, 0.975))$summary
   ## check that dimensions are correct
   expect_equal(dim(mod_summ), c(34, 7))
   ## check that mean of mu for taxon 1 is "close" to mean qPCR for taxon 1
-  expect_equal(mean(mod_summ[grepl("mu", rownames(mod_summ)) & grepl(",1]", rownames(mod_summ)), 1]), mean(processed_data$qpcr[, 1]), tolerance = 0.3)
+  expect_equal(mean(mod_summ[grepl("mu", rownames(mod_summ)) & grepl(",1]", rownames(mod_summ)), 1]), mean(example_qPCR_data[, 1]), tolerance = 0.3)
 })
