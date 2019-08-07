48904b3c3654d25081616bf6f8939b27e0815de2
Brian Williamson <brianw26@uw.edu>
2019-08-07 15:14:49 -0700

    change man pages, document data

---
+++ R/example_16S_data.R
@@ -0,0 +1,11 @@
+#' Example relative abundance data
+#'
+#' A dataset inspired by data collected by the University of Washington (UW) 
+#' Sexually Transmitted Infections Cooperative Research Center (STICRC). Relative abundance, as measured by 
+#' broad range 16S sequencing with "universal" primers, is available on 20 samples and 433 taxa, and is  
+#' is provided in `example_16S_data`. 
+#' 
+#' @format `example_16S_data`: a matrix with 20 rows and 433 columns, 
+#' where each column is the relative abundance of a particular taxon in a given sample (row).
+#' @source Inspired by data collected for Project 2 of the UW STICRC \url{http://depts.washington.edu/cfas/research/sexually-transmitted-infections-cooperative-research-center/projects/establishing-microbial-and-biochemical-thresholds-for-development-and-persistence-of-bacterial-vaginosis/}.
+"example_16S_data"
---
+++ R/example_qPCR_data.R
@@ -0,0 +1,11 @@
+#' Example abolute abundance data
+#'
+#' A dataset inspired by data collected by the University of Washington (UW) 
+#' Sexually Transmitted Infections Cooperative Research Center (STICRC). Absolute abundance, 
+#' as measured by taxon-specific absolute primers,
+#' is available on 20 samples but only 7 taxa, and is provided in `example_qPCR_data`.
+#' 
+#' @format `example_qPCR_data`: a matrix with 20 rows and 7 columns, where each column is the absolute abundance
+#' of a particular taxon in a given sample (row).
+#' @source Inspired by data collected for Project 2 of the UW STICRC \url{http://depts.washington.edu/cfas/research/sexually-transmitted-infections-cooperative-research-center/projects/establishing-microbial-and-biochemical-thresholds-for-development-and-persistence-of-bacterial-vaginosis/}.
+"example_qPCR_data"
