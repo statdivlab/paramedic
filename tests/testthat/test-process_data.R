4d7853b706e1037b53b673b7fd8140e3bba61fc3
Brian Williamson <brianw26@uw.edu>
2019-08-08 14:55:02 -0700

    fix process_data test

--- tests/testthat/test-process_data.R
+++ tests/testthat/test-process_data.R
@@ -51,7 +51,7 @@ test_that("processing works", {
   processed_data <- paramedic::process_data(full_data = simple_example_data, rel_inds = 1:q,
                                             abs_inds = (q + 1):(q + q_obs),
                                             abs_plus_rel_inds = 1:q_obs,
-                                            regex_thr = "", regex_abs = "", llod = 0,
+                                            regex_thr = "", regex_abs = "_cps", llod = 0,
                                             m_min = 1000, div_num = 1)
   expect_equal(dim(processed_data$relative)[2], q)
   expect_equal(dim(processed_data$absolute)[2], q_obs)
