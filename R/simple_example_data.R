b19c03f5476ced1c27e4f8396fd254f4d723993f
Brian Williamson <brianw26@uw.edu>
2019-08-08 14:03:49 -0700

    add back in the simple example data, for processing

---
+++ R/simple_example_data.R
@@ -0,0 +1,7 @@
+#' Simulated data from relative and absolute abundance views
+#'
+#' A simple simulated dataset, to show how we might process prior to running \code{run_paramedic}.
+#' 
+#' @format `simple_example_data`: a matrix with 10 rows and 5 columns, 
+#' where the first three columns are relative abundance data and the final two columns are absolute abundance data.
+"simple_example_data"
